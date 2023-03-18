4441a018a7076a6821cfa73bb327afbc940b1a9b
Reid-T-W <rediettadesse100@gmail.com>
2023-03-18 15:08:26 +0300

    login and registration page complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./App.scss";
+// import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Pages
+import { Home, Contact, Login, Register, Reset } from "./pages";
+// Components
+import { Header, Footer } from "./components"
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/reset" element={<Reset />}/>
+        </Routes>
+      <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
